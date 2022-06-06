2b4fcdbb366ff9235e0a7ca55f5fa67749f88c08
Robotix <82544307+realRobotix@users.noreply.github.com>
2022-06-06 16:42:17 +0200

    fixed loading extensions

--- gott.py
+++ gott.py
@@ -1,4 +1,5 @@
 import os
+from posixpath import dirname
 import disnake
 import disnake.ext
 from disnake.ext import commands
@@ -14,49 +15,56 @@ bot = commands.Bot(command_prefix='!',
                    test_guilds=[970711821478686721], intents=disnake.Intents.all(), auto_sync=True)
 
 
-@bot.slash_command(name="extensions", description="used to configure extensions", channel_types=disnake.ChannelType.private)
-async def extensions(inter):
+@bot.slash_command(name="extensions", description="used to configure extensions")
+async def extensions(inter: disnake.ApplicationCommandInteraction):
     if inter.author.id not in config.BOT_DEVELOPERS:
-        await inter.response.send_message("you are not a bot developer")
+        await inter.response.send_message("only bot developers can use this command", ephemeral=True)
         return
 
 
 @extensions.sub_command(name="load", description="load an extension. use ```all``` to load all extensions")
 async def extensions_load(inter: disnake.ApplicationCommandInteraction, extension: str) -> None:
     if inter.author.id not in config.BOT_DEVELOPERS:
-        await inter.response.send_message("only bot developers can use this command")
+        await inter.response.send_message("only bot developers can use this command", ephemeral=True)
         return
     if extension == "all":
-        for (dirpath, filenames) in os.walk("./extensions"):
+        for (dirpath, dirname, filenames) in os.walk("./extensions"):
             for file in filenames:
                 fullPath = os.path.join(dirpath, file)
                 if file.endswith(".py") and not file.startswith("_"):
-                    extension = fullPath[:-
-                                         3].replace("/", ".").replace("\\", ".")
+                    extension = "extensions." + fullPath[:-
+                                                         3].replace("/", ".").replace("\\", ".").split("extensions.")[1]
                     try:
-                        bot.load_extension(f"extensions.{extension}")
-                        await inter.response.send_message(f"loaded {extension}")
+                        bot.load_extension(extension)
+                        await inter.response.send_message(f"loaded {extension}", ephemeral=True)
                     except Exception as e:
-                        await inter.response.send_message(f"failed to load {extension}\n{e}")
+                        await inter.response.send_message(f"failed to load {extension}\n{e}", ephemeral=True)
         return
-    bot.load_extension(f"extensions.{extension}")
-    await inter.response.send_message(f"loaded {extension}")
+    try:
+        bot.load_extension(f"extensions.{extension}")
+        await inter.response.send_message(f"loaded {extension}", ephemeral=True)
+    except Exception as e:
+        await inter.response.send_message(f"failed to load {extension}\n{e}", ephemeral=True)
 
 
 @extensions.sub_command(name="unload", description="unload an extension. use ```all``` to unload all extensions")
 async def extensions_unload(inter: disnake.ApplicationCommandInteraction, extension: str) -> None:
     if inter.author.id not in config.BOT_DEVELOPERS:
-        await inter.response.send_message("you are not a bot developer")
+        await inter.response.send_message("only bot developers can use this command", ephemeral=True)
         return
     if extension == "all":
         for extension in bot.extensions:
             try:
                 bot.unload_extension(extension)
-                await inter.response.send_message(f"unloaded {extension}")
+                await inter.response.send_message(f"unloaded {extension}", ephemeral=True)
             except Exception as e:
-                await inter.response.send_message(f"failed to unload {extension}\n{e}")
-    bot.unload_extension(f"commands.{extension}")
-    await inter.response.send_message(f"unloaded {extension}")
+                await inter.response.send_message(f"failed to unload {extension}\n{e}", ephemeral=True)
+        return
+    try:
+        bot.unload_extension(f"extensions.{extension}")
+        await inter.response.send_message(f"unloaded {extension}", ephemeral=True)
+    except Exception as e:
+        await inter.response.send_message(f"failed to unload {extension}\n{e}", ephemeral=True)
 
 
 @extensions.sub_command(name="reload", description="reload an extension. use ```all``` to reload all extensions")
@@ -68,12 +76,15 @@ async def extensions_reload(inter: disnake.ApplicationCommandInteraction, extens
         for extension in bot.extensions:
             try:
                 bot.reload_extension(extension)
-                await inter.response.send_message(f"reloaded {extension}")
+                await inter.response.send_message(f"reloaded {extension}", ephemeral=True)
             except Exception as e:
-                await inter.response.send_message(f"failed to reload {extension}\n{e}")
-    bot.reload_extension(f"commands.{extension}")
-    await inter.response.send_message(f"reloaded {extension}")
-
+                await inter.response.send_message(f"failed to reload {extension}\n{e}", ephemeral=True)
+        return
+    try:
+        bot.reload_extension(f"extension.{extension}")
+        await inter.response.send_message(f"reloaded {extension}", ephemeral=True)
+    except Exception as e:
+        await inter.response.send_message(f"failed to reload {extension}\n{e}", ephemeral=True)
 if __name__ == "__main__":
     try:
         main()
