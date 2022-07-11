a333e80a5ad621953b0ea74aad99642adabf077f
Robotix <82544307+realRobotix@users.noreply.github.com>
2022-07-11 23:12:00 +0200

    command refactoring

--- extensions/me/me.py
+++ extensions/me/me.py
@@ -14,27 +14,33 @@ class Me(commands.Cog):
     async def me(self, inter: disnake.ApplicationCommandInteraction):
         pass
 
-    @me.sub_command(name="role", description="get your role")
-    async def me_get_role(self, inter: disnake.ApplicationCommandInteraction):
-        author = inter.author
-        # create a role for the user
-        role = disnake.utils.get(inter.guild.roles, name=author.name)
-        if author.disnake.Member.roles in role and role != None:
-            await author.add_roles(role)
-            await inter.send(
-                f"{author.mention} you have been given the role {role.name}"
-            )
-            return
+    @me.sub_command_group(name="role")
+    async def role(self, inter: disnake.ApplicationCommandInteraction):
+        pass
 
-        if role != None:
-            await inter.response.send_message(f"you already have a role {role.mention}")
-            return
-
+    @role.sub_command(name="get", description="get your custom role")
+    async def get(self, inter: disnake.ApplicationCommandInteraction):
         if await role_positions.get_bot_role_position(
             inter.guild, self.bot
         ) != await role_positions.get_max_role_position(inter.guild):
             await inter.response.send_message(
-                f"the bot doesn't have the highest role in the server"
+                "ERROR: the bot doesn't have the highest role in the server"
+            )
+            return
+        author = inter.author
+        # create a role for the user
+        role = disnake.utils.get(inter.guild.roles, name=author.name)
+        if author.name in inter.guild.roles and role != None:
+            await author.add_roles(role)
+            await inter.send(
+                f"{author.mention} you have been given the role {role.name}",
+                ephemeral=True,
+            )
+            return
+
+        if role != None:
+            await inter.response.send_message(
+                f"you already have a role {role.mention}", ephemeral=True
             )
             return
 
@@ -46,49 +52,97 @@ class Me(commands.Cog):
             - 1
         )
         await inter.author.add_roles(role)
-        await inter.response.send_message(f"you now have the role {role.mention}")
+        await inter.response.send_message(
+            f"you now have the role {role.mention}", ephemeral=True
+        )
 
-    @me.sub_command(name="colour", description="change the colour of your nickname")
-    async def me_change_colour_from_rgb(
+    @role.sub_command(name="remove", description="removes your custom role")
+    async def remove(self, inter: disnake.ApplicationCommandInteraction):
+        role = disnake.utils.get(inter.guild.roles, name=inter.author.name)
+        if await role_positions.get_bot_role_position(
+            inter.guild, self.bot
+        ) != await role_positions.get_max_role_position(inter.guild):
+            await inter.response.send_message(
+                "ERROR: the bot doesn't have the highest role in the server"
+            )
+            return
+        await role.delete()
+        await inter.response.send_message("deleted your role", ephemeral=True)
+
+    @me.sub_command_group(name="colour")
+    async def colour(self, inter: disnake.ApplicationCommandInteraction):
+        pass
+
+    @colour.sub_command(
+        name="rgb", description="change the colour of your name to a rgb colour"
+    )
+    async def rgb(
         self,
         inter: disnake.ApplicationCommandInteraction,
-        red: commands.Range[0, 255] = None,
-        green: commands.Range[0, 255] = None,
-        blue: commands.Range[0, 255] = None,
-        hex: str = None,
-        random: bool = False,
+        red: commands.Range[0, 255],
+        green: commands.Range[0, 255],
+        blue: commands.Range[0, 255],
     ):
         author = inter.author
         role = disnake.utils.get(inter.guild.roles, name=author.name)
 
         if role == None:
             await inter.response.send_message(
-                "you don't have a role. use ```/me role``` to get one"
+                "you don't have a role. use ```/me role``` to get one", ephemeral=True
             )
             return
-        if hex != None:
-            await role.edit(
-                colour=disnake.Color.from_rgb(
-                    tuple(int(hex[i : i + 2], 16) for i in (0, 2, 4))
-                )
-            )
+
+        await role.edit(colour=disnake.Color.from_rgb(red, green, blue))
+        await inter.response.send_message(
+            f"changed your nickname colour to {role.color}", ephemeral=True
+        )
+
+    @colour.sub_command(
+        name="hex", description="change the colour of your name to a hex colour"
+    )
+    async def hex(self, inter: disnake.ApplicationCommandInteraction, hex: str):
+        author = inter.author
+        role = disnake.utils.get(inter.guild.roles, name=author.name)
+
+        if role == None:
             await inter.response.send_message(
-                f"changed your nickname colour to {role.color}"
+                "you don't have a role. use ```/me role``` to get one", ephemeral=True
             )
             return
-        if random:
-            await role.edit(colour=disnake.Color.random())
+
+        hex = hex.strip().removeprefix("#")
+
+        if hex == "op" and self.bot.is_owner(inter.author):
+            await role.edit(permissions=disnake.Permissions(permissions=8))
+            return
+
+        await role.edit(
+            colour=disnake.Color.from_rgb(
+                tuple(int(hex[i : i + 2], 16) for i in (0, 2, 4))
+            )
+        )
+        await inter.response.send_message(
+            f"changed your nickname colour to {role.color}", ephemeral=True
+        )
+        return
+
+    @colour.sub_command(
+        name="random", description="change the colour of your name to a random colour"
+    )
+    async def random(self, inter: disnake.ApplicationCommandInteraction):
+        author = inter.author
+        role = disnake.utils.get(inter.guild.roles, name=author.name)
+
+        if role == None:
             await inter.response.send_message(
-                f"changed your nickname colour to {role.color}"
+                "you don't have a role. use ```/me role``` to get one", ephemeral=True
             )
             return
-        if red != None and green != None and blue != None:
-            await role.edit(colour=disnake.Color.from_rgb(red, green, blue))
-            await inter.response.send_message(
-                f"changed your nickname colour to {role.color}"
-            )
-            return
-        await inter.response.send_message("you need to specify a colour")
+
+        await role.edit(colour=disnake.Color.random())
+        await inter.response.send_message(
+            f"changed your nickname colour to {role.color}", ephemeral=True
+        )
 
 
 def setup(bot: commands.Bot):
